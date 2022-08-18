6485fed45816bd36ffa3ceb4ec59663283f2e955
sueys <sueys@live.fr>
2022-08-19 00:58:58 +0200

    todolist test 01

---
+++ script.js
@@ -0,0 +1,36 @@
+"use strict"
+
+ol.innerHTML = localStorage.getItem("list");
+
+const spanDels = document.querySelectorAll(".delete");
+
+for(let span of spanDels)
+{
+    span.onclick = () => del(span.parentElement)
+}
+noTache.style.display = (ol.innerHTML == "")? "block" : "none";
+
+form.onsubmit = () =>{
+const li = document.createElement("li");
+const spanDel = document.createElement("span");
+spanDel.classList.add("delete");
+spanDel.textContent = "[x]";
+spanDel.onclick = () => del(li);
+li.innerHTML = champ.value;
+
+li.appendChild(spanDel);
+ol.appendChild(li);
+
+champ.value = "";
+noTache.style.display = "none";
+localStorage.setItem("list", ol.innerHTML);
+//event.preventDefault();
+return false;
+}
+
+function del(element)
+{
+    element.remove();
+    noTache.style.display = (ol.innerHTML == "")? "block" : "none";
+    localStorage.setItem("list", ol.innerHTML);
+}
